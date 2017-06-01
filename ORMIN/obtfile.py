b5fdfd2b4235c07b05010956ccfa48e18bf984cd
fmatara <Lordfernandus@gmail.com>
2017-06-01 21:21:48 +0200

    Update obtfile.py

--- ORMIN/obtfile.py
+++ ORMIN/obtfile.py
@@ -10,15 +10,14 @@ class InvalidOs(Exception):
 
 osname = os.name
 if osname == 'posix':
-    files = str(subprocess.check_output("dir", shell=True)).replace("b\'", "")
-    files = files.replace("\\n'", "").split()
-    for file in files:
-        if '.rmin' not in file:
-            files.remove(file)
-        else:
-            pass
+    files = str(subprocess.check_output("ls", shell=True)).replace("b\'", "")
+    files = files.replace("\\\\n", " ")
+    files = files.split()
+    files = str(files).replace("\\\\n", " ").split()
+    files[0] = files[0].replace("[\"", "")
+    files.remove(files[-1])
     print(files)
-    
+
 elif osname == 'nt':
     print("Windows is not suported yet")
 else:
