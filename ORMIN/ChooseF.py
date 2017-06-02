fae4ce7392b7539258967abfe651532c3b5d2eb6
fmatara <Lordfernandus@gmail.com>
2017-06-02 10:52:58 +0200

    Update ChooseF.py

--- ORMIN/ChooseF.py
+++ ORMIN/ChooseF.py
@@ -1,1 +1,20 @@
 #choosing file script
+def choosef(files):
+    global choice
+    n = 1
+    for file in files:
+        print("%i. %s" % (n, file))
+        n += 1
+
+    while True:
+        choice = input("Choose the file\n")
+        try:
+            int(choice)
+            if int(choice) <= len(files):
+                print(files[int(choice)-1])
+                break
+        except:
+            if file == choice:
+                break
+            else:
+                print("no se ha encontrado el archivo")
